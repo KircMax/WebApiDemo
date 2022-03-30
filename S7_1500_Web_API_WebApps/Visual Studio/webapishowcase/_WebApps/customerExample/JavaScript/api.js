39c3031c9a9ba7cbca45a0b8bd86f5ce96c12b5b
Kirchberger, Max (DI CS SD EH PA 1) <max.kirchberger@siemens.com>
2022-03-30 17:30:38 +0200

    Added cookies in Api.js in customerExample

--- S7_1500_Web_API_WebApps/Visual Studio/webapishowcase/_WebApps/customerExample/JavaScript/api.js
+++ S7_1500_Web_API_WebApps/Visual Studio/webapishowcase/_WebApps/customerExample/JavaScript/api.js
@@ -52,13 +52,25 @@ function keepSession(token, pingInterval) {
         keepSession(token);
     }, pingInterval);
 }
-function setCookie(cname, cvalue, exminutes) {
-    //example for one working implementation can be found at: https://www.w3schools.com/js/js_cookies.asp
-    alert("setcookie not implemented - can be found at: https://www.w3schools.com/js/js_cookies.asp");
-}
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
 function getCookie(cname) {
-    //example for one working implementation can be found at: https://www.w3schools.com/js/js_cookies.asp
-    alert("getcookie not implemented - can be found at: https://www.w3schools.com/js/js_cookies.asp");
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
     return "";
 }
 function updateTurbineSpeed() {
