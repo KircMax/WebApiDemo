c2fde1d683529e3540527f64454f366f40d4564d
Kirchberger, Max (DI CS SD EH PA 1) <max.kirchberger@siemens.com>
2022-07-03 12:19:54 +0200

    keep "old" setup an environment

--- SIMATIC_WebAPI_Set_up_an_Environment/Visual Studio/UnitTest1.cs
+++ SIMATIC_WebAPI_Set_up_an_Environment/Visual Studio/UnitTest1.cs
@@ -7,7 +7,6 @@ using Siemens.Simatic.S7.Webserver.API.Services.FileParser;
 using Siemens.Simatic.S7.Webserver.API.Services.WebApp;
 using Siemens.Simatic.S7.Webserver.API.Enums;
 using Siemens.Simatic.S7.Webserver.API.Services;
-using System.IO;
 
 namespace WebApiDemo
 {
@@ -26,36 +25,6 @@ namespace WebApiDemo
                 Console.WriteLine(method.Name);
             }
         }
-
-        [Test]
-        public async Task Api_WebAppDeployer_KeepUpdatingTheApp()
-        {
-            var serviceFactory = new ApiStandardServiceFactory();
-            var requestHandler = serviceFactory.GetApiHttpClientRequestHandler(IPAddress, Username, Password);
-
-            var appDir = Path.Combine(CurrentExeDir.Parent.Parent.FullName, "_WebApps", "customerExample");
-            var configParser = new ApiWebAppConfigParser(appDir, "WebAppConfig.json", new ApiWebAppResourceBuilder());
-            var app = configParser.Parse();
-
-            var deployer = serviceFactory.GetApiWebAppDeployer(requestHandler);
-            //for a full deploy: requestHandler.WebAppDelete(app);
-            while (true)
-            {
-                app = configParser.Parse();
-                await deployer.DeployOrUpdateAsync(app);
-                Thread.Sleep(100);
-                var apps = requestHandler.WebAppBrowse().Result.Applications;
-                var appResources = requestHandler.WebAppBrowseResources(app).Result.Resources;
-            }
-        }
-
-        public static DirectoryInfo CurrentExeDir
-        {
-            get
-            {
-                string dllPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
-                return (new FileInfo(dllPath)).Directory;
-            }
-        }
+        
     }
 }
